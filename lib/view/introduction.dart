b4275b737d6b6c2f88ea35b53adab822753839d5
Amir-HShahi <amir13831107h@gmail.com>
2024-12-03 17:20:28 +0330

    implement start button handler

--- lib/view/introduction.dart
+++ lib/view/introduction.dart
@@ -8,6 +8,10 @@ class Introduction extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void tapStartButtonHandler() {
+      Navigator.pushNamed(context, "/home");
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -29,9 +33,11 @@ class Introduction extends StatelessWidget {
                     color: const Color(0xff5F33E1)),
               ),
               SizedBox(height: scaledHeight(72)),
-               BottomFloatingButton(
+              BottomFloatingButton(
                   title: "Let’s Start",
-                  iconPath: "assets/arrow_left.svg", placeIconAtEnd: true, onTap: () {  })
+                  iconPath: "assets/arrow_left.svg",
+                  placeIconAtEnd: true,
+                  onTap: tapStartButtonHandler)
             ],
           )
         ],
