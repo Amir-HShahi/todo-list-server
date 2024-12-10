4edd585f7daf02dac7b9e58f46793a0a66300176
Amir-HShahi <amir13831107h@gmail.com>
2024-12-10 18:57:06 +0330

    define task model

--- lib/model/task_model.dart
+++ lib/model/task_model.dart
@@ -1,3 +1,15 @@
 class TaskModel {
+  final int id;
+  String title;
+  String description;
+  DateTime createdAt;
+  DateTime dueDate;
+  bool isCompleted = false;
 
+  TaskModel(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.createdAt,
+      required this.dueDate});
 }
