361401b9253efc1a01a2ce4f7aa08e58e336a2c6
Amir-HShahi <amir13831107h@gmail.com>
2024-12-12 17:01:09 +0330

    complete view model

--- lib/viewModel/task_view_model.dart
+++ lib/viewModel/task_view_model.dart
@@ -3,11 +3,30 @@ import 'package:todo_list_server/model/task_model.dart';
 import 'package:todo_list_server/services/database_handler.dart';
 
 class TaskViewModel with ChangeNotifier {
-  List<TaskModel> taskModels = [];
+  late List<TaskModel> _taskModels = [];
 
-  Future<void> addTaskModel(TaskModel taskModel) async {
+  void addTaskModel(TaskModel taskModel) {
     DatabaseHandler.insertTaskModel(taskModel);
-    taskModels.add(taskModel);
+    _taskModels.add(taskModel);
+    notifyListeners();
+  }
+
+  List<TaskModel> getTaskModels() {
+    return _taskModels;
+  }
+
+  void initializeData() async {
+    _taskModels = await DatabaseHandler.retrieveTaskModels();
+    notifyListeners();
+  }
+
+  void deleteTaskModel(TaskModel taskModel) async {
+    DatabaseHandler.deleteTaskModel(taskModel);
+    notifyListeners();
+  }
+
+  void editTaskModel(TaskModel taskModel) async {
+    DatabaseHandler.updateTaskModel(taskModel);
     notifyListeners();
   }
 }
