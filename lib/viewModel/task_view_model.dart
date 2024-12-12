9cd98c84f50af570e4ba56f014a405ee4ac11b00
Amir-HShahi <amir13831107h@gmail.com>
2024-12-12 16:53:57 +0330

    add using view model

--- lib/viewModel/task_view_model.dart
+++ lib/viewModel/task_view_model.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:todo_list_server/model/task_model.dart';
+import 'package:todo_list_server/services/database_handler.dart';
 
 class TaskViewModel with ChangeNotifier {
+  List<TaskModel> taskModels = [];
 
+  Future<void> addTaskModel(TaskModel taskModel) async {
+    DatabaseHandler.insertTaskModel(taskModel);
+    taskModels.add(taskModel);
+    notifyListeners();
+  }
 }
