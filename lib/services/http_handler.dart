5550fc50c9013a35c049c12151686de3a6259584
Amir-HShahi <amir13831107h@gmail.com>
2024-12-19 13:43:39 +0330

    init http handler

--- lib/services/http_handler.dart
+++ lib/services/http_handler.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:todo_list_server/model/task_model.dart';
 
 class HttpHandler {
+  static const String _ipAddress = '127.0.0.1';
+  static const String _port = '666';
+
   static void insertTaskModel(TaskModel taskModel) async {
 
   }
