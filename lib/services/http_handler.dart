f2ffa3f64a55948e47f24f6c7b9e47e26b928d9a
Amir-HShahi <amir13831107h@gmail.com>
2024-12-19 13:27:38 +0330

    init http handler

---
+++ lib/services/http_handler.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:todo_list_server/model/task_model.dart';
+
+class HttpHandler {
+  static void insertTaskModel(TaskModel taskModel) async {
+
+  }
+
+  static void deleteTaskModel(TaskModel taskModel) async {
+
+  }
+
+  static void updateTaskModel(TaskModel taskModel) async {
+
+  }
+
+  static void getTaskModel() async {
+
+  }
+
+  static FutureOr<List<TaskModel>> retrieveTaskModels() async {
+    return [];
+  }
+}
