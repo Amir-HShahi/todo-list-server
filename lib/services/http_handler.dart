58fdf95fb83a0580643a88b8ddcff2e6177d5e3d
Amir-HShahi <amir13831107h@gmail.com>
2024-12-19 15:55:17 +0330

    insert task update

--- lib/services/http_handler.dart
+++ lib/services/http_handler.dart
@@ -8,11 +8,9 @@ class HttpHandler {
   static const String _ipAddress = '10.0.2.2';
   static const String _port = '666';
 
-  static void insertTaskModel(TaskModel taskModel) async {
-    await http.post(Uri.parse('http://$_ipAddress:$_port/to_do/tasks/'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
+  static Future<http.Response> insertTaskModel(TaskModel taskModel) async {
+    return http.post(Uri.parse('http://$_ipAddress:$_port/to_do/tasks/'),
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
         body: jsonEncode(taskModel.toJson()));
   }
 
